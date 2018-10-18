0a0e4d483f977cea4f90f0a870636a19334fc66f
dragon <43591718+koumori0115@users.noreply.github.com>
2018-10-18 16:47:56 +0900

    更新②

---
+++ Assets/Scripts/Serch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Serch : MonoBehaviour {
+    GameObject check;
+    Tilemap tilemap;
+    // Use this for initialization
+    void Start () {
+        Tilemap tilemap = GetComponent<Tilemap>();
+        Debug.Log(tilemap.GetSprite(new Vector3Int(-9, 13, 0)));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void serch(Vector3Int serchposition)
+    {
+        if(tilemap.GetInstantiatedObject(serchposition) != null)
+            Debug.Log(tilemap.GetInstantiatedObject(serchposition));
+    }
+}
