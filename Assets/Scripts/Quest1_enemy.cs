976bae52ad61d149279ad6657d08a1a610fe98fa
missingno0083 <43591807+missingno0083@users.noreply.github.com>
2018-12-06 16:05:07 +0900

    クエストマップ１

---
+++ Assets/Scripts/Quest1_enemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quest1_enemy : MonoBehaviour {
+
+    public GameObject text1;     
+    public GameObject text2;
+    public GameObject text3;
+    private int count;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+
+
+    //プレイヤー触れたら
+    void OnTriggerEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //count += 1;
+            text1.SetActive(true);
+        }
+
+        if (col.gameObject.tag == "Player" && count == 1)
+        {
+            count += 1;
+            text2.SetActive(true);
+        }
+
+        if (col.gameObject.tag == "Player" && count == 2)
+        {
+            count += 1;
+            text3.SetActive(true);
+        }
+    }
+
+    //オブジェクトが離れたタイミング
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+            text1.SetActive(false);
+            text2.SetActive(false);
+            text3.SetActive(false);
+        }
+
+    }
+
+}
