0e3fc8c23c9fa397d3bf6907f984fa85e1655aa8
dragon <43591718+koumori0115@users.noreply.github.com>
2019-01-30 09:47:52 +0900

    Revert "ああ"
    This reverts commit 2e39ed8039f0e0ec2ee84645788c3c054c223ceb.

---
+++ Assets/Scripts/Quest_Script/Quest4_nonda.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest4_nonda : ItemsandChara
+{
+    [SerializeField] GameObject money;
+    [SerializeField] string objectName;
+    // Use this for initialization
+    public override void Start()
+    {
+        base.Start();
+        set_eventText(new string[] { "泥棒？ああ、最近噂の奴か。生憎だが俺は知らねぇぞ。" });
+        set_nomalText(new string[] {"だから、俺は知らねぇって" });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void eventResult()
+    {
+        base.eventResult();
+        event_flag = false;
+        GameObject.Find("GameManager").GetComponent<Item_List>().setUseItems(money);
+    }
+
+}
