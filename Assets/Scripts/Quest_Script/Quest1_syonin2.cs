c12dc493ebf0fc6b0e7f3bb270276796ba67390a
dragon <43591718+koumori0115@users.noreply.github.com>
2019-02-04 11:09:41 +0900

    スクリプト更新

---
+++ Assets/Scripts/Quest_Script/Quest1_syonin2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest1_syonin2 : ItemsandChara
+{
+
+    int i = 0;
+
+    // Use this for initialization
+    public override void Start()
+    {
+        base.Start();
+        set_eventText(new string[] { "", "" });
+        set_nomalText(new string[] { "" });
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public override void eventResult()
+    {
+        base.eventResult();
+    }
+
+
+}
